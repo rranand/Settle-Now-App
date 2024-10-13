d3e38accba8b1d38b56e3ec537b7c6c869055ce8
Rohit Anand <rohitanand3336@gmail.com>
2024-10-13 14:22:49 +0530

    Floating Button Improved

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -4433,7 +4433,7 @@ class _DashBoardState extends State<DashBoard> {
                         child: SizedBox(
                           height: (MediaQuery.of(context).size.height - 220),
                           child: open == 0
-                              ? !quickSplitData.value.isEmpty
+                              ? quickSplitData.value.isEmpty
                                   ? (quickSplitDataFetched
                                       ? Scrollbar(
                                           radius: Radius.circular(10.0),
@@ -4622,7 +4622,7 @@ class _DashBoardState extends State<DashBoard> {
                                         subCategory: subCategory,
                                       ))
                               : open == 1
-                                  ? !RoomDataO.value.isEmpty
+                                  ? RoomDataO.value.isEmpty
                                       ? activeRoomDataFetched
                                           ? Scrollbar(
                                               radius: Radius.circular(10.0),
@@ -4679,7 +4679,7 @@ class _DashBoardState extends State<DashBoard> {
                                                         : 2),
                                             membersData: membersData,
                                           ))
-                                  : (!RoomDataC.value.isEmpty
+                                  : (RoomDataC.value.isEmpty
                                       ? inActiveRoomDataFetched
                                           ? Scrollbar(
                                               radius: Radius.circular(10.0),
