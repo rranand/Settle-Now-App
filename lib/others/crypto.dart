636edd8a8c348c06dda64d394225f89f33c33ea2
Rohit Anand <rrohitanand3336@gmail.com>
2025-02-09 21:23:04 +0530

    Minor Issues Resolved

--- lib/others/crypto.dart
+++ lib/others/crypto.dart
@@ -1,18 +1,21 @@
+import 'dart:convert';
 import 'package:encrypt/encrypt.dart';
 import '../contents.dart' as global;
 
 class crypto {
-  static final _key = Key.fromUtf8(global.crypto_key);
-  static final _iv = IV.fromUtf8(global.crypto_iv);
-
-  static final encrypter = Encrypter(AES(_key, mode: AESMode.cbc));
+  static final Key _key =
+      Key(utf8.encode(global.crypto_key)); // 32-byte key for AES-256
+  static final IV _iv =
+      IV(utf8.encode(global.crypto_iv)); // 16-byte IV for CBC mode
 
   static String encrypt(String text) {
+    final encrypter = Encrypter(AES(_key, mode: AESMode.cbc));
     final encrypted = encrypter.encrypt(text, iv: _iv);
     return encrypted.base64;
   }
 
   static String decrypt(String text) {
+    final encrypter = Encrypter(AES(_key, mode: AESMode.cbc));
     final decrypted = encrypter.decrypt64(text, iv: _iv);
     return decrypted;
   }
