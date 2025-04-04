5a172753819ac3bc3cfc85c38c7247dd1dec00a6
Rohit Anand <rrohitanand3336@gmail.com>
2025-04-05 03:17:08 +0530

    Bottom Navigation Bar Added In Home

---
+++ lib/constant/home_ui_constant.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+const List<String> drawerTitle = [
+  "Profile",
+  "Bank Transactions",
+  "Reminder",
+  "Import Contacts",
+  "Get Notified",
+  "Theme",
+  "Share",
+  "About Us",
+  "Contact Us",
+  "Rate Us",
+  "Log Out",
+];
+
+const List<IconData> drawerIcon = [
+  Iconsax.profile_2user,
+  Iconsax.bank,
+  Iconsax.calendar_tick,
+  Iconsax.import,
+  Icons.notifications_active_outlined,
+  Iconsax.sun_1,
+  Iconsax.share,
+  Iconsax.archive_book,
+  Iconsax.support,
+  Iconsax.star,
+  Iconsax.logout,
+];
+
+const List<String> bottomNavigationButtonText = [
+  "Home",
+  "Personal",
+  "Len-Den",
+  "Analysis",
+];
+const List<IconData> bottomNavigationButtonIcon = [
+  Iconsax.home,
+  Iconsax.wallet_1,
+  Iconsax.money_change,
+  Iconsax.graph,
+];
--- lib/screen/dashboard/home/home_screen.dart
+++ lib/screen/dashboard/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:settlenow_v2/constant/home_ui_constant.dart';
 import 'package:settlenow_v2/provider/screen_size_provider.dart';
 import 'package:settlenow_v2/screen/bank_transactions/bank_transaction_screen.dart';
 import 'package:settlenow_v2/util/widgets/image_widget.dart';
@@ -15,6 +15,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   EdgeInsets _mainScreenPadding = EdgeInsets.zero;
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    if (mounted) {
+      _selectedIndex = index;
+      setState(() {});
+    }
+  }
 
   @override
   void didChangeDependencies() {
@@ -41,6 +49,35 @@ class _HomeScreenState extends State<HomeScreen> {
           },
         ),
       ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+          boxShadow: const [
+            BoxShadow(color: Colors.black12, blurRadius: 2, spreadRadius: 1),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+          child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            selectedItemColor: Colors.deepPurpleAccent,
+            unselectedItemColor: Colors.black54,
+            selectedFontSize: 12,
+            unselectedFontSize: 10,
+            showUnselectedLabels: true,
+            type: BottomNavigationBarType.fixed,
+            items: List.generate(
+              bottomNavigationButtonText.length,
+              (index) => BottomNavigationBarItem(
+                icon: Icon(bottomNavigationButtonIcon[index]),
+                label: bottomNavigationButtonText[index],
+              ),
+            ),
+          ),
+        ),
+      ),
       drawer: Drawer(
         child: ListView(
           children: [
@@ -52,151 +89,40 @@ class _HomeScreenState extends State<HomeScreen> {
               accountName: Text("Rohit Anand"),
               accountEmail: Text("rrohitanand3336@gmail.com"),
             ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(
-                Iconsax.profile_2user,
-                color: Colors.white,
-                size: 22,
-              ),
-              title: Text(
-                "Profile",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Iconsax.bank, color: Colors.white, size: 22),
-              title: Text(
-                "Bank Transactions",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-              trailing: Container(
-                width: 55,
-                height: 30,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
+            ...List.generate(drawerTitle.length, (index) {
+              return ListTile(
+                onTap: () {},
+                leading: Icon(drawerIcon[index], color: Colors.white, size: 22),
+                title: Text(
+                  drawerTitle[index],
+                  style: TextStyle(fontSize: 14, color: Colors.white),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    "Beta",
-                    style: TextStyle(fontSize: 13, color: Colors.white),
+                trailing: Visibility(
+                  visible: index == 3 || index == 4,
+                  child: Container(
+                    width: 55,
+                    height: 30,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(color: Colors.white60),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        "Beta",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(
-                Iconsax.calendar_tick,
-                color: Colors.white,
-                size: 22,
-              ),
-              title: Text(
-                "Reminder",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-              trailing: Container(
-                width: 55,
-                height: 30,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    "Beta",
-                    style: TextStyle(fontSize: 13, color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Iconsax.import, color: Colors.white, size: 22),
-              title: Text(
-                "Import Contacts",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(
-                Icons.notifications_active_outlined,
-                color: Colors.white,
-                size: 22,
-              ),
-              title: Text(
-                "Get Notified",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Iconsax.sun_1, color: Colors.white, size: 22),
-              title: Text(
-                "Theme",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.brightness_2, color: Colors.black87, size: 22),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Iconsax.share, color: Colors.white, size: 22),
-              title: Text(
-                "Share",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(
-                Iconsax.archive_book,
-                color: Colors.white,
-                size: 22,
-              ),
-              title: Text(
-                "About Us",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Iconsax.support, color: Colors.white, size: 22),
-              title: Text(
-                "Contact Us",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(
-                Icons.star_border_rounded,
-                color: Colors.white,
-                size: 22,
-              ),
-              title: Text(
-                "Rate Us",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
-            ListTile(
-              onTap: () async {},
-              leading: Icon(Iconsax.logout, color: Colors.white, size: 22),
-              title: Text(
-                "Log Out",
-                style: TextStyle(fontSize: 14, color: Colors.white),
-              ),
-            ),
+              );
+            }),
             ListTile(
               title: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
