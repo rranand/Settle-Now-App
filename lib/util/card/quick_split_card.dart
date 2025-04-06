d34c49b152adae92cba751a8b1af9c79f2d6e7be
Rohit Anand <rrohitanand3336@gmail.com>
2025-04-06 21:45:32 +0530

    QuickSplit Info Added

--- lib/util/card/quick_split_card.dart
+++ lib/util/card/quick_split_card.dart
@@ -14,7 +14,81 @@ class QuickSplitCard extends StatefulWidget {
 
 class _QuickSplitCardState extends State<QuickSplitCard> {
   final double amount = -100;
-  final bool isExpanded = true;
+  final ValueNotifier<bool> isExpanded = ValueNotifier(false);
+
+  Widget transactionInfoWidget(bool value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Visibility(
+          visible: !value,
+          child: const SizedBox(height: UiConstant.cardSpaceAfterSubText),
+        ),
+        Visibility(
+          visible: !value,
+          child: overlapUserImageWidget(
+            context,
+            UiConstant.memberAvatars,
+            4,
+            totalUsers: UiConstant.memberAvatars.length,
+            imageRadius: 30,
+            nextImageOffset: 24,
+          ),
+        ),
+        Visibility(
+          visible: !value,
+          child: const SizedBox(height: UiConstant.cardSpaceAfterSubText),
+        ),
+        subTextOnCard("Created By Rohit Anand"),
+        subTextOnCard("Created On March 10, 2025"),
+        Visibility(
+          visible: value,
+          child: subTextOnCard("Modified On June 10, 2025"),
+        ),
+      ],
+    );
+  }
+
+  Widget extendedTransactionWidget() {
+    return Column(
+      children: [
+        Divider(),
+        ListView.separated(
+          shrinkWrap: true,
+          itemCount: 3,
+          itemBuilder: (BuildContext context, int index) {
+            double memberAmount =
+                (100 + 10 * index) * (index % 2 == 0 ? -1 : 1);
+            String imgUrl =
+                UiConstant.memberAvatars[index %
+                    UiConstant.memberAvatars.length];
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    overlapUserImageWidget(context, [imgUrl], 1),
+                    SizedBox(width: 8),
+                    Text("Rohit Anand"),
+                  ],
+                ),
+                Text(
+                  formatCurrency(memberAmount, context),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: memberAmount < 0 ? Colors.red : Colors.green,
+                  ),
+                ),
+              ],
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider(thickness: 0.3);
+          },
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +123,12 @@ class _QuickSplitCardState extends State<QuickSplitCard> {
                   ),
                 ],
               ),
-              Icon(Iconsax.info_circle, color: Colors.grey),
+              InkWell(
+                child: Icon(Iconsax.info_circle, color: Colors.grey),
+                onTap: () {
+                  isExpanded.value = !isExpanded.value;
+                },
+              ),
             ],
           ),
           Row(
@@ -67,34 +146,15 @@ class _QuickSplitCardState extends State<QuickSplitCard> {
                           "Purchased a burger",
                           style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        Visibility(
-                          visible: !isExpanded,
-                          child: const SizedBox(
-                            height: UiConstant.cardSpaceAfterSubText,
-                          ),
-                        ),
-                        Visibility(
-                          visible: !isExpanded,
-                          child: overlapUserImageWidget(
-                            context,
-                            UiConstant.memberAvatars,
-                            4,
-                            totalUsers: UiConstant.memberAvatars.length,
-                            imageRadius: 30,
-                            nextImageOffset: 24,
-                          ),
-                        ),
-                        Visibility(
-                          visible: !isExpanded,
-                          child: const SizedBox(
-                            height: UiConstant.cardSpaceAfterSubText,
-                          ),
-                        ),
-                        subTextOnCard("Created By Rohit Anand"),
-                        subTextOnCard("Created On March 10, 2025"),
-                        Visibility(
-                          visible: isExpanded,
-                          child: subTextOnCard("Modified On June 10, 2025"),
+                        ValueListenableBuilder(
+                          valueListenable: isExpanded,
+                          builder: (
+                            BuildContext context,
+                            bool value,
+                            Widget? child,
+                          ) {
+                            return transactionInfoWidget(value);
+                          },
                         ),
                       ],
                     ),
@@ -111,46 +171,16 @@ class _QuickSplitCardState extends State<QuickSplitCard> {
               ),
             ],
           ),
-          Visibility(
-            visible: isExpanded,
-            child: Column(
-              children: [
-                Divider(),
-                ListView.separated(
-                  shrinkWrap: true,
-                  itemCount: 3,
-                  itemBuilder: (BuildContext context, int index) {
-                    double memberAmount =
-                        (100 + 10 * index) * (index % 2 == 0 ? -1 : 1);
-                    String imgUrl =
-                        UiConstant.memberAvatars[index %
-                            UiConstant.memberAvatars.length];
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            overlapUserImageWidget(context, [imgUrl], 1),
-                            SizedBox(width: 8),
-                            Text("Rohit Anand"),
-                          ],
-                        ),
-                        Text(
-                          formatCurrency(memberAmount, context),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: memberAmount < 0 ? Colors.red : Colors.green,
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return Divider(thickness: 0.3);
-                  },
-                ),
-              ],
-            ),
+          ValueListenableBuilder(
+            valueListenable: isExpanded,
+            builder: (context, value, child) {
+              if (value) {
+                return child!;
+              } else {
+                return SizedBox.shrink();
+              }
+            },
+            child: extendedTransactionWidget(),
           ),
         ],
       ),
