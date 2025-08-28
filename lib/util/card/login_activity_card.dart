8a56aa3295614d90c3fc1a2596bf2f4fad8659d4
Rohit Anand <rrohitanand3336@gmail.com>
2025-08-29 02:58:37 +0530

    Custom Shimmer Effect Added in login activity

--- lib/util/card/login_activity_card.dart
+++ lib/util/card/login_activity_card.dart
@@ -6,6 +6,7 @@ import 'package:settlenow_v2/util/enum/device_type.dart';
 import 'package:settlenow_v2/util/enum/enums.dart';
 import 'package:settlenow_v2/util/functions/text_function.dart';
 import 'package:settlenow_v2/util/widgets/button_with_shimmer_effect.dart';
+import 'package:settlenow_v2/util/widgets/custom_button.dart';
 import 'package:settlenow_v2/util/widgets/shimmer_effect.dart';
 import 'package:settlenow_v2/util/widgets/snackbar.dart';
 import 'package:settlenow_v2/util/widgets/widgets.dart';
@@ -16,65 +17,74 @@ class LoginActivityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (!data.hasData) {
-      return CustomShimmerEffect.placeHolderShimmerEffect(
-        Container(
-          height: 100,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16),
-          ),
-        ),
-      );
-    }
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListTile(
-          leading: colouredIcon(
-            DeviceTypeExtension.fromString(data.deviceType).icon,
-            DeviceTypeExtension.fromString(data.deviceType).color,
-          ),
-          title: Text(data.deviceName),
-          subtitle: Text.rich(
-            TextSpan(
-              text: data.deviceType,
-              style: TextStyle(fontSize: 12, color: Colors.grey),
-              children: [
-                TextSpan(text: "\nActive "),
-                TextSpan(text: convertToMoment(data.lastLoggedIn)),
-              ],
-            ),
-          ),
-          trailing:
-              data.id.isEmpty
-                  ? ButtonWithShimmerEffect(
-                    buttonText: "Current",
-                    buttonType: CustomButtonType.customElevatedButton,
-                    isLoaded: false,
-                    buttonHeight: 40,
-                    buttonWidth: 100,
-                    backgroundColor: Colors.green.shade400,
-                    onPressed: () {
-                      showNormalSnackBar(
-                        context,
-                        "Can't Logout Current Device",
-                      );
-                    },
+          leading:
+              data.hasData
+                  ? colouredIcon(
+                    DeviceTypeExtension.fromString(data.deviceType).icon,
+                    DeviceTypeExtension.fromString(data.deviceType).color,
                   )
-                  : ButtonWithShimmerEffect(
-                    buttonText: "Log Out",
-                    buttonType: CustomButtonType.customElevatedButton,
-                    isLoaded: false,
-                    buttonHeight: 40,
-                    buttonWidth: 100,
-                    backgroundColor: Colors.red.shade400,
-                    onPressed: () {
-                      context.read<UserLoginActivityCubit>().logoutDevice(
-                        context,
-                        data.id,
-                      );
-                    },
+                  : CustomShimmerEffect.imageWidget(
+                    shape: BoxShape.circle,
+                    radius: 50,
+                  ),
+          title:
+              data.hasData
+                  ? Text(data.deviceName)
+                  : CustomShimmerEffect.textWidget(width: 80),
+          subtitle:
+              data.hasData
+                  ? Text.rich(
+                    TextSpan(
+                      text: data.deviceType,
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                      children: [
+                        TextSpan(text: "\nActive "),
+                        TextSpan(text: convertToMoment(data.lastLoggedIn)),
+                      ],
+                    ),
+                  )
+                  : CustomShimmerEffect.textWidget(fontSize: 10, width: 80),
+          trailing:
+              data.hasData
+                  ? (data.id.isEmpty
+                      ? ButtonWithShimmerEffect(
+                        buttonText: "Current",
+                        buttonType: CustomButtonType.customElevatedButton,
+                        isLoaded: false,
+                        buttonHeight: 40,
+                        buttonWidth: 100,
+                        backgroundColor: Colors.green.shade400,
+                        onPressed: () {
+                          showNormalSnackBar(
+                            context,
+                            "Can't Logout Current Device",
+                          );
+                        },
+                      )
+                      : ButtonWithShimmerEffect(
+                        buttonText: "Log Out",
+                        buttonType: CustomButtonType.customElevatedButton,
+                        isLoaded: false,
+                        buttonHeight: 40,
+                        buttonWidth: 100,
+                        backgroundColor: Colors.red.shade400,
+                        onPressed: () {
+                          context.read<UserLoginActivityCubit>().logoutDevice(
+                            context,
+                            data.id,
+                          );
+                        },
+                      ))
+                  : CustomShimmerEffect.placeHolderShimmerEffect(
+                    CustomButton.customElevatedButton(
+                      "",
+                      buttonHeight: 40,
+                      buttonWidth: 100,
+                    ),
                   ),
         ),
       ),
